f6b2fddb149927bbf62033bc3383224cdd815a47
kundan <kundanbora77@gmail.com>
2019-02-23 16:54:04 +0530

    Chart View Added

--- app/src/main/java/com/triplethree/slytherine/MainActivity.java
+++ app/src/main/java/com/triplethree/slytherine/MainActivity.java
@@ -1,8 +1,11 @@
 package com.triplethree.slytherine;
 
+import android.graphics.Canvas;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.TextureView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +15,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.db.chart.model.ChartSet;
+import com.db.chart.model.LineSet;
+import com.db.chart.view.LineChartView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -31,6 +42,17 @@ public class MainActivity extends AppCompatActivity
                         .setAction("Action", null).show();
             }
         });
+        Canvas canvas  = new Canvas();
+        String[] labels = {"monday","tuesday","wednesday"};
+        float  [] values = {10f,20f,15f};
+
+
+        LineSet dataset = new LineSet(labels,values);
+        LineChartView lineChartView = findViewById(R.id.batteryLineChart);
+
+
+        lineChartView.onAttachedToWindow();
+        lineChartView.onDrawChart(canvas,);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
